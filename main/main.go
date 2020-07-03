973d4637adde20882ee5cb29a130a437f69969ee
yuni prihartini <yunicendikia97@gmail.com>
2020-07-03 09:58:31 +0700

    [Modify Conflicts]

--- main/main.go
+++ main/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func printName() {
-	fmt.Println("dinda maharani :(, Viontina")
+	fmt.Println("dinda maharani :(, Viontina Dea I")
 }
 func main() {
 	fmt.Println("Hello World")
